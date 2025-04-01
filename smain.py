65e6fb2e3553fb301dc1c73dea492a19069c5635
Inqvis <gordololero202@gmail.com>
2025-04-01 17:31:17 -0300

    Calculadora

--- smain.py
+++ smain.py
@@ -1,4 +1,31 @@
-print("dsadas")
+def Multiplicacion():
+    resultado = chokko * wafle
+    print(resultado)
+def Division():
+    resultado = chokko / wafle
+    print(resultado)
+def Suma():
+    resultado = chokko + wafle
+    print(resultado)
+def Resta():
+    resultado = chokko - wafle
+    print(resultado)
 
-print("dsadas")
-print("dsadas")
+
+     
+print("Calculadora")
+chokko = float(input("Ingrese el primer valor: "))
+Monoko = input("Ingrese el operador: ")
+wafle = float(input("Ingrese el segundo valor: "))
+if Monoko == "*":
+    Multiplicacion()
+if Monoko == "/":
+    if wafle == 0:
+        print("No se divide entre 0")
+        exit
+    else:
+       Division() 
+if Monoko == "+":
+    Suma()
+if Monoko == "-":
+    Resta()
